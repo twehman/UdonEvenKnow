f6c0fbd924c6eb15898b12dd2fe69c51b913d094
Shanygne Campo <campo.shanygne@gmail.com>
2020-04-07 10:37:10 -0400

    Updated database for user preferences

---
+++ vue_frontend-starter_java/backend-server/database/usersdata.sql
@@ -0,0 +1,18 @@
+BEGIN TRANSACTION;
+
+drop table if exists users_data;
+
+create table users_data (
+users_id serial PRIMARY KEY
+,id integer NOT NULL
+,first_name varchar NOT NULL
+,last_name varchar NOT NULL
+,address varchar NOT NULL
+,city varchar NOT NULL
+,state varchar NOT NULL
+,zip_code integer NOT NULL
+);
+
+ALTER TABLE users_data ADD FOREIGN KEY (id) REFERENCES users(id);
+
+COMMIT;
