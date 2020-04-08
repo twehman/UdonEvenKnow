a90b3cf3484993c20eb9b5abf9cf04b4231529bb
David Weinberger <davidsweinberger@gmail.com>
2020-04-08 11:56:39 -0400

    Buttons Components

---
+++ vue_frontend-starter_java/frontend-Vue/src/components/Home/DislikeButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="dislike-button">
+    <button class="btn btn-danger" v-on:click="DislikeRestaurant">Dislike</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DislikeButton",
+  methods: {
+    DislkeRestaurant() {
+      this.$emit("Dislike");
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue_frontend-starter_java/frontend-Vue/src/components/Home/LikeButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="like-container">
+    <button id="like" class="btn btn-primary" v-on:click="LikeRestaurant">Like</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LikeButton",
+  methods: {
+    LikeRestaurant() {
+      this.$emit("Like");
+    }
+  }
+};
+</script>
+
+<style>
+div.like-container {
+  display: inline-block;
+}
+</style>
