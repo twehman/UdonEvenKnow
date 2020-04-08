a082f2eafe7d66022c10ab73209798e028ece54b
Thomas Wehman <twehman@gmail.com>
2020-04-08 13:57:39 -0400

    made cals

--- vue_frontend-starter_java/frontend-Vue/src/views/Restaurants.vue
+++ vue_frontend-starter_java/frontend-Vue/src/views/Restaurants.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-<p>{{this.zipcode.latitude}} </p>
+<p v-for="item in cuisines">{{item}} </p>
 </div>
 </template>
 
@@ -15,6 +15,7 @@ export default {
         latitude: '',
         longitude: '',
       },
+      cuisines: []
     };
   },
   methods: {
@@ -35,10 +36,25 @@ export default {
           this.zipcode = userZipcode
       })
       .catch((err) => console.log(err))
+    fetch('https://developers.zomato.com/api/v2.1/cuisines?lat=41.480881&lon=-81.80036', {
+              method: 'GET',
+              headers: {
+                Accept: 'application/json',
+                'user-key': 'c1bbb3341d92fcff2ad26d1965e26008',
+              }
+        })
+        .then((response) =>{
+            return response.json()
+        })
+        .then((data) => {
+            this.cuisines = data
+        })
+        .catch((err) => console.log(err))
 
-  }
-};
+}
+}
 </script>
 
 <style>
 </style>
+
