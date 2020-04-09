925f5ba62b281ca0cd0b57cfea6c366fcbd64e89
Thomas Wehman <twehman@gmail.com>
2020-04-09 11:47:39 -0400

    Logout

---
+++ vue_frontend-starter_java/frontend-Vue/src/components/Home/Logout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <b-button id="logout" variant="danger" v-on:click="Logout">Logout</b-button>
+  </div>
+</template>
+
+<script>
+import auth from '@/auth'
+export default {
+  name: "logout",
+  methods: {
+    Logout() {
+      fetch(`${process.env.VUE_APP_REMOTE_API}/restaurants`, {
+              method: 'POST',
+              headers: new Headers({
+                Authorization: 'Bearer ' + auth.getToken(),
+              }),
+              credentials: 'same-origin',
+            })
+        .then((response) => {
+            if(response.ok) {
+                auth.logout()
+                this.$router.push({ path: '/login' });
+            }
+        })
+    }
+  }
+};
+</script>
+
+<style>
+div.like-container {
+  display: inline-block;
+}
+</style>
