a2cb301d9dd067d5f85276f7fd7214c7f555870f
Shanygne Campo <campo.shanygne@gmail.com>
2020-04-15 10:04:39 -0400

    Added animation

--- vue_frontend-starter_java/frontend-Vue/src/views/Home.vue
+++ vue_frontend-starter_java/frontend-Vue/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <b-container fluid>
 <div class="main">
-    <h1 class="sign" align="center">UD<b-icon-question-circle />N EVEN KNOW</h1>
+    <h1 class="sign" align="center">UD<b-icon-question-circle animation="spin" />N EVEN KNOW</h1>
     <h3 align="center">Let's help you figure out where to eat</h3>
 <div class="pref">
     <p><h2 class="h2" align="left"> Select your preferred cuisine types:</h2></p>
